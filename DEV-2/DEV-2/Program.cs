d2fcf9256101b9e6e238a24c87b98e793c51c87f
Anton Redzkin <anton.redzkin@gmail.com>
2017-02-13 16:14:43 +0300

    Calculae expression
    Calculae expression using method 'DataTable.Compute'

---
+++ DEV-2/DEV-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DEV_2
+{
+    class Program
+    {
+        // This program calcualate math expession
+        // which supported operations : "+", "-", "*", "/" and
+        // support brackets.
+
+        static void Main(string[] args)
+        {
+            char again = 'y';
+            while (again == 'y' || again == 'Y')
+            {
+                Expression expr = new Expression();
+                expr.inputExpression();
+                expr.calculateExpression(expr.checkExpression());
+                Console.WriteLine("Do you want to continue? (y/n)");
+                again = Convert.ToChar(Console.ReadLine());
+            }
+        }
+    }
+}
