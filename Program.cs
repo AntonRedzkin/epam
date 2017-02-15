62e6f962bcba98ea9490e46252039105fd22db61
Anton Redzkin <anton.redzkin@gmail.com>
2017-02-15 16:46:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserConsole1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kones = true;
+            ArrayList all = new ArrayList();
+
+            while (kones)
+            {
+                Console.WriteLine();
+                OutputInf.PrintMessage();
+                int a = int.Parse(Console.ReadLine());
+                if (a == 1)
+                {
+                    EnterUser.inputUserData(all);
+                }
+                else if (a == 2)
+                {
+                    OutputInf.PrintAllStudent(all);
+                }
+                else if (a == 3)
+                {
+                    OutputInf.AveregeAge(all);
+                }
+                else if (a == 4)
+                {
+                    OutputInf.OldestUser(all);
+                }
+                else if (a == 5)
+                {
+                    OutputInf.PopularWomanName(all);
+                }
+                else kones = false;
+            }
+        }
+        
+    }
+}
---
+++ User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserConsole1
+{
+    class User
+    {
+        public string name;
+        public string surname;
+        public string sex;
+        public int age;
+        public override string ToString()
+        {
+            return string.Format(" Name:{0}, Surname:{1}, Sex:{2}, Age:{3}", name, surname, sex, age);
+        }
+
+    }
+}
