e44b5ae8df50bc114b00d7cb4a070b4ad46790f0
Anton Redzkin <anton.redzkin@gmail.com>
2017-02-26 15:36:40 +0200

    Add Dev-6

---
+++ Dev-6/DateTimeFormatInputer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dev_6
+{
+    class DateTimeFormatInputer
+    {
+        public string Input()
+        {
+            Console.WriteLine("Input Date Time Format: ");
+            return Console.ReadLine();
+        }
+    }
+}
