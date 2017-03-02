44930ff32651971f00a7288d0cb8796756c60eec
Anton Redzkin <anton.redzkin@gmail.com>
2017-03-02 15:24:14 +0200

    DEV-8

---
+++ Dev-8/Inputer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dev_8
+{
+    class Inputer
+    {
+        public void CreateMatrix(string name, out double[,] matrix)
+        {
+            Console.WriteLine("Enter the dimension of matrix {0}: ", name);
+            matrix = new double[Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write("{0}[{1},{2}] = ", name, i, j);
+                    matrix[i, j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
---
+++ Dev-8/Outputer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dev_8
+{
+    class Outputer
+    {
+        public void Print(string name, double[,] matrix)
+        {
+            Console.WriteLine("\nMatrix {0}:", name);
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
