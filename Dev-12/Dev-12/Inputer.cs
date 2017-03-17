364fb6e72803a05fbbda05c6ba66669ab4894d7f
Anton Redzkin <anton.redzkin@gmail.com>
2017-03-17 16:00:10 +0200

    Add files via upload

---
+++ Dev-12/Dev-12/Inputer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Dev_12
+{
+    class Inputer
+    {
+        public const int BLACK = 1;
+        public const int WHITE = 0;
+        private string startData;
+
+        public void ReadData(Checkers ch1, Checkers ch2)
+        {
+            Console.WriteLine("Input color (w, b): ");
+            ch1.Color = ParseColor(Console.ReadLine().ToLower());
+            Console.WriteLine("Input startPosition (a1 - h8): ");
+            ch1.CurrentPosition = ParsePosition(Console.ReadLine().ToLower());
+            ch1.positionX = ch1.CurrentPosition.First();
+            ch1.positionY = ch1.CurrentPosition.Last();
+            ch2 = ch1;
+            Console.WriteLine("Input endPosition (a1 - h8): ");
+            ch2.CurrentPosition = ParsePosition(Console.ReadLine().ToLower());
+        }
+
+        public int ParseColor(string colorString)
+        {
+            int color = -1;
+            while (color < 0) 
+            {                
+                if (colorString == "b" || colorString == "black")
+                {
+                    color = BLACK;
+                }
+                else if (colorString == "w" || colorString == "white")
+                {
+                    color = WHITE;
+                }
+                else
+                {
+                    Console.WriteLine("Error. Invalid format. Do you want to input color? (y, n)");
+                    if (Console.ReadLine() == "n")
+                    {
+                        break;
+                    }
+                }
+            }
+            return color;
+        }
+
+        public int[] ParsePosition(string input)
+        {
+            int[] Position = new int[2];
+            string coordinatePattern = @"^\s*[abcdefgh]\s*[12345678]\s*$";
+            if (Regex.IsMatch(input, coordinatePattern))
+            {
+                input = input.Trim();
+                if ((input.First() - 96) % 2 == input.Last() % 2)
+                {
+                    Position[0] = input.First() - 96;
+                    Position[1] = input.Last() - 48;
+                    return Position;
+                }                    
+                else
+                    throw new FormatException("Only black fields are allowed");
+            }
+            else
+                throw new FormatException("Wrong format of coordinate");
+        }
+
+    }
+}
---
+++ Dev-12/Dev-12/StepsCalculater.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev_12
+{
+    class StepsCalculater
+    {
+        private static String WRONG_POSITION_MSG = "WRONG position ";
+        public const int BLACK = 1;
+        public const int WHITE = 0;
+
+        public void Count(Checkers newChecker, Board newBoard)
+        {
+            int ICurrentPosition = newChecker.CurrentPosition.First();
+            int JCurrentPosition = newChecker.CurrentPosition.Last();
+            int IWantedPosition = newChecker.WantedPosition.First();
+            int JWantedPosition = newChecker.WantedPosition.Last();
+
+            newBoard.square[ICurrentPosition, JCurrentPosition].State = 1;
+
+        }
+
+        public int Move(Checkers ch1, Checkers ch2)
+        {
+
+            if (!isValid(ch1, ch2))
+            {
+                throw new ArgumentException(WRONG_POSITION_MSG);
+            }
+
+            if (ch1.Color == WHITE)
+            {
+                return goWhite(ch1, ch2);
+            }
+            else
+            {
+                return goBlack(ch1, ch2);
+            }
+        }
+
+        private bool isValid(Checkers ch1, Checkers ch2)
+        {
+            bool isValid = true;
+
+            if (!(ch1.Color == ch2.Color))
+            {
+                isValid = false;
+            }
+
+            if (ch1.Color==WHITE)
+            {
+                if ((ch1.positionX - ch1.positionY < ch2.positionX - ch2.positionY) ||
+                    (ch1.positionX + ch1.positionY > ch2.positionX + ch2.positionY))
+                {
+                    isValid = false;
+                }
+            }
+            else
+            {
+                if ((ch1.positionX - ch1.positionY > ch2.positionX - ch2.positionY) ||
+                    (ch1.positionX + ch1.positionY < ch2.positionX + ch2.positionY))
+                {
+                    isValid = false;
+                }
+            }
+            return isValid;
+        }
+
+        private int goWhite(Checkers ch1, Checkers ch2)
+        {
+            int steps = 0;
+
+            if (ch1.positionX < ch2.positionX)
+            {
+                while (!ch1.areSame(ch2))
+                {
+                    while (ch1.positionX + ch1.positionY != ch2.positionX + ch2.positionY)
+                    {
+                        ch1.goRight();
+                        steps++;
+                    }
+                    ch1.goLeft();
+                    steps++;
+                }
+            }
+            else
+            {
+                while (!ch1.areSame(ch2))
+                {
+                    while (ch1.positionX - ch1.positionY != ch2.positionX - ch2.positionY)
+                    {
+                        ch1.goLeft();
+                        steps++;
+                    }
+                    ch1.goRight();
+                    steps++;
+                }
+            }
+            return steps;
+        }
+
+        private int goBlack(Checkers ch1, Checkers ch2)
+        {
+            int steps = 0;
+
+            if (ch1.positionX < ch2.positionX)
+            {
+                while (!ch1.areSame(ch2))
+                {
+                    while (ch1.positionX - ch1.positionY != ch2.positionX - ch2.positionY)
+                    {
+                        ch1.goRight();
+                        steps++;
+                    }
+                    ch1.goLeft();
+                    steps++;
+                }
+            }
+            else
+            {
+                while (!ch1.areSame(ch2))
+                {
+                    while (ch1.positionX + ch1.positionY != ch2.positionX + ch2.positionY)
+                    {
+                        ch1.goLeft();
+                        steps++;
+                    }
+                    ch1.goRight();
+                    steps++;
+                }
+            }
+            return steps;
+        }
+    }
+}
