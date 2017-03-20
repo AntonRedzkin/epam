8bf0f351cf00ca1d3341ac97bf2a4a82c37a96a3
Anton Redzkin <anton.redzkin@gmail.com>
2017-03-20 16:42:45 +0300

    DEV-12 with tests

---
+++ Dev-12/ExceptionHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dev_12
+{
+    public class ExceptionHandler
+    {
+        public void HandleAndQuestion(Exception exception, out bool key)
+        {
+            Console.WriteLine("{0}", exception.Message);
+            Console.WriteLine("Try again?(y/n)");
+            string ansver = Console.ReadLine().ToLower();
+            if (ansver.Equals("y"))
+            {
+                key = true;
+            }
+            else
+            {
+                key = false;
+            }            
+        }
+    }
+}
