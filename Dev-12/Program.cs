2768f68baa8f395e47250520453567f27c642291
Anton Redzkin <anton.redzkin@gmail.com>
2017-03-20 16:38:09 +0300

    DEV-12 with Tests

---
+++ Dev-12/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Dev_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idOfWantedSquare = 0;
+            Checkers checker = new Checkers();
+            Inputer inputer = new Inputer();
+            Board board = new Board();
+            ExceptionHandler exceptionHandler = new ExceptionHandler();
+            bool key = true;
+            while (key)
+            {
+                try
+                {
+                    inputer.GetData(checker, out idOfWantedSquare);
+                    key = false;
+                }
+                catch (FormatException exception)
+                {
+                    exceptionHandler.HandleAndQuestion(exception, out key);
+                }
+            }
+            StepsCalculater stepsCalculater = new StepsCalculater();
+            try
+            {                
+                int step = stepsCalculater.Move(checker, board, board.Select(idOfWantedSquare));
+                Console.WriteLine("Steps: {0}", step);
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine("{0}", exception.Message);
+            }
+        }
+    }
+}
---
+++ Dev-12/Square.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Dev_12
+{
+    public class Square
+    {
+        public int Color { get; set; }
+        public int State { get; set; }
+        public int Id { get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+    }
+}
